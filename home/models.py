1ccf2d507fdcc247e2c6c220e47e02a746d193f5
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2018-04-27 02:44:14 +0530

    Minor Fix

--- home/models.py
+++ home/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
+	email = models.CharField(max_length = 50)
 	username = models.CharField(max_length = 50)
 	password = models.CharField(max_length = 20)
